7dcfb3b205d0a92cb51d481f9f573d6da6f3b40f
Ajeet <Ajeet@Lucifer>
2016-04-08 23:24:27 +0530

    Simple block breaker game.

---
+++ src/game/Animate.java
@@ -0,0 +1,19 @@
+package game;
+
+public class Animate implements Runnable {
+    BlockBreakerPanel bp;
+    Animate(BlockBreakerPanel b){
+        bp=b;
+    }
+    @Override
+    public void run(){
+        while(true){
+            bp.update();
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
